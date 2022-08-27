9ec4a596fbf55f35835c77c44c9f76c8ed58474e
Couttolenc01 <105402763+Couttolenc01@users.noreply.github.com>
2022-08-26 22:42:49 -0500

    Create Activity.h

---
+++ Activity.h
@@ -0,0 +1,67 @@
+// =================================================================
+//
+// File: activity.h
+// Author:Francisco Couttolenc Ortiz
+// Date: 26/08/22
+//
+// =================================================================
+
+
+#ifndef ACTIVITY_H
+#define ACTIVITY_H
+#include <iostream>
+using namespace std;
+
+// =================================================================
+// sumaIterativa. Calculate the sum from 1 to n with an iterative
+// method.
+//
+// @param n	The maximum limit of the sum.
+// @return	The result of the addition from 1 to n.
+// @Complexity	
+// =================================================================
+	unsigned int sumaIterativa(unsigned int n) {
+		int suma;
+		for(int i=1;i<=n;i++)
+		{
+			suma += i;
+		}
+		return suma;
+	}
+
+// =================================================================
+// sumaRecursiva. Calculate the sum from 1 to n with an recursive
+// method.
+//
+// @param n	The maximum limit of the sum.
+// @return	The result of the addition from 1 to n.
+// @Complexity	
+// =================================================================
+unsigned int sumaRecursiva(unsigned int n) {
+	int suma;
+	if (n == 1) 
+	{
+		suma = 1;
+	}
+	else 
+	{
+		suma = n + sumaRecursiva(n - 1);
+	}
+	return suma;
+}
+
+// =================================================================
+// sumaDirecta. Calculate the sum from 1 to n with a mathematical
+// method.
+//
+// @param n	The maximum limit of the sum.
+// @return	The result of the addition from 1 to n.
+// @Complexity	
+// =================================================================
+unsigned int sumaDirecta(unsigned int n) {
+  int suma = (n * (n + 1));
+	return suma;
+}
+
+
+#endif /* ACTIVITY_H */
